484a94a7f3f089538ade465710e1988c013a146a
alisa-lu <alisasl@uci.edu>
2024-02-01 21:44:53 -0800

    change broadcast to use same port everytime

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 def listen_for_broadcasts(my_port):
     """Listens for broadcast messages on the local network and responds to them."""
@@ -19,17 +20,27 @@ def listen_for_broadcasts(my_port):
         response_message = f"Hello {sender_port}, I am {my_port}".encode()
         sock.sendto(response_message, addr)  # Send response to the sender's address and port
 
-def send_broadcast():
-    """Sends a broadcast message on the local network."""
+def send_broadcast(my_port, target_port):
+    """Sends a broadcast message on the local network from a specific port."""
     message = b'Hello, peers!'
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.sendto(message, ('<broadcast>', 12345))
+    
+    # Bind the socket to a specific port number
+    sock.bind(('', my_port))  # Bind to my_port for sending
+    
+    # Send the message to the target_port on all devices in the broadcast address
+    sock.sendto(message, ('<broadcast>', target_port))
+    sock.close()
 
 if __name__ == "__main__":
-    my_port = 12345  # This is your port number for listening and responding
-    listener_thread = threading.Thread(target=listen_for_broadcasts, args=(my_port,))
+    my_port = 12345  # Port from which to send messages
+    target_port = 12345  # Target port to which messages are sent
+    listener_thread = threading.Thread(target=listen_for_broadcasts, args=(target_port,))
     listener_thread.start()
 
-    # Optionally, send a broadcast message to discover peers
-    send_broadcast()
+    # Wait a moment for the listener to be ready (optional)
+    time.sleep(1)
+
+    # Send a broadcast message
+    send_broadcast(my_port, target_port)
