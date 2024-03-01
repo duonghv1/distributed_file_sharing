751e348fa3721250e55dc083b1b5a622cbd4542c
duonghv1 <thuyduong8122002@gmail.com>
2024-02-29 21:33:56 -0800

    added test script for load testing

---
+++ test_automate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define the log file path
+LOG_FILE="output.log"
+
+# Redirect stdout and stderr to the log file
+exec > >(tee -a ${LOG_FILE} )
+exec 2>&1
+
+# Prompt user for the number of nodes
+read -p "Enter the number of nodes you want to start: " NUM_NODES
+
+# Check if the input is valid
+if ! [[ "$NUM_NODES" =~ ^[0-9]+$ ]]; then
+    echo "Error: Please enter a valid number."
+    read -p "Enter the number of nodes you want to start: " NUM_NODES
+fi
+
+SERVER_BASE_PORT=12345
+BASE_DIR="./files"
+
+
+# Execute the Python script with provided arguments
+for ((i=0; i<=$NUM_NODES; i++)); do
+    PORT=$((SERVER_BASE_PORT + i))
+    DIR="$BASE_DIR_$PORT"
+    python3 FileServer.py --port="$PORT" --base_dir="$DIR" &
+done
+
